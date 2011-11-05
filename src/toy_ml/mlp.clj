213051735b6d7ec9a0e57f930116ef66467ef9aa
Andy Wu <icetortoise@gmail.com>
2011-11-05 22:49:42 +0800

    support arbitary hidden layer

--- src/toy_ml/mlp.clj
+++ src/toy_ml/mlp.clj
@@ -2,15 +2,19 @@
 (use '(toy-ml core))
 (use '(incanter core))
 
-;; todo, linear/softmax...
-;; todo, predict, momentem; confmax; early stop
+;; todo, momentem; confmax; early stop or regularization?
 
 (defn initial-weights [inputs targets hidden]
   (defn- rand-init [n] (- (rand (/ 2 (sqrt n))) (/ 1 (sqrt n))))
-  (let [input_dim (+ 1 (ncol inputs))]
-    [(m-map rand-init (matrix input_dim input_dim hidden))
-     (m-map rand-init (matrix (+ 1 hidden) (+ 1 hidden)
-                              (ncol targets)))]))
+  (defn- each-adj-pair [col]
+    (drop-last (map (fn [x y] [x y])
+                    col (conj (vec (rest col)) (first col)))))
+  (defn- make-random-matrix [[n1 n2]]
+    (m-map rand-init (matrix (+ 1 n1) (+ 1 n1) n2)))
+  (let [nnodes (flatten [(ncol inputs)
+                         hidden
+                         (ncol targets)])]
+    (map make-random-matrix (each-adj-pair nnodes))))
 
 (defn intecept [inputs]
   (bind-columns (matrix -1 (nrow inputs) 1)
@@ -20,43 +24,78 @@
   (trans (rest (trans x))))
 
 (defn make-logistic [beta]
-  (fn [x] (/ 1 (+ 1 (exp (- (* beta x)))))))
+  {:forward (fn [x] (/ 1 (+ 1 (exp (- (* beta x))))))
+   :backward (fn [targets outputs]
+               (mult (minus targets outputs)
+                        outputs
+                        (minus 1 outputs)))})
 
-(defn mlp-forward [inputs w_hidden w_out beta]
-  (let [inputs (intecept inputs)
-        logistic (make-logistic beta)
-        activations (m-map logistic (mmult inputs w_hidden))
-        outputs (m-map logistic (mmult (intecept activations)
-                                       w_out))]
-    [activations outputs]))
+(defn make-linear []
+  {:forward identity
+   :backward (fn [targets outputs]
+               (div (minus targets outputs)
+                    (nrow outputs)))})
+
+;; skipping softmax implementation
+(comment (defn make-softmax []
+  {:forward identity
+  :backward (fn [targets outputs]
+               (div (minus targets outputs)
+                    (nrow outputs)))}))
+
+(defn mlp-forward [inputs weights forward-fn]
+  (defn- compute-activations [previous weight]
+    (conj previous
+          (m-map forward-fn
+                 (mmult (intecept (last previous))
+                        weight))))
+  (rest (reduce compute-activations
+                (cons [inputs] weights))))
+
+(defn- hidden-act-weight-pairs
+  [acts weights]
+  (reverse
+   (map (fn [act weight]
+          [act weight])
+        (drop-last acts)
+        (rest weights))))
+
+(defn- h-delta [prev-deltas [act weight]]
+  (conj prev-deltas
+        (unintecept (mult (intecept act) (minus 1 (intecept act))
+                          (mmult (last prev-deltas) (trans weight))))))
+
+(defn- new-weight [weight delta input-or-hidden-act]
+  (plus weight
+        (mmult (trans (intecept input-or-hidden-act))
+               delta)))
 
 (defn mlp-backward
-  [inputs targets outputs hidden_act w_hidden w_out]
-  (let [delta_out (mult (minus targets outputs)
-                        outputs
-                        (minus 1 outputs))
-        delta_hidden (mult (intecept hidden_act) (minus 1 (intecept hidden_act))
-                           (mmult delta_out (trans w_out)))]
-    [(plus w_hidden (mmult (trans (intecept inputs)) (unintecept delta_hidden)))
-     (plus w_out (mmult (trans (intecept hidden_act)) delta_out))]
-  ))
+  ([inputs targets activations weights backward-fn]
+     (let [delta-out (backward-fn targets (last activations))
+           h-deltas (reduce h-delta
+                            (cons [delta-out]
+                                  (hidden-act-weight-pairs
+                                   activations weights)))]
+       (map new-weight
+            weights
+            (reverse (cons delta-out h-deltas))
+            (cons inputs (drop-last activations))))))
 
-(defn mlp-train [inputs targets hidden beta momentum out-type end-fn]
-  (let [[weights_hidden weights_out]
-        (initial-weights inputs targets hidden)]
-    (loop [w_hidden weights_hidden
-           w_out weights_out
+(defn mlp-train [inputs targets hidden momentum act-fns end-fn]
+  (let [weights (initial-weights inputs targets hidden)]
+    (loop [weights weights
            inputs inputs
            targets targets
            iter 1]
-      (let [[activations outputs]
-            (mlp-forward inputs w_hidden w_out beta)
-            [w_hidden_new w_out_new]
-            (mlp-backward inputs targets outputs activations
-                          w_hidden w_out)]
-        (if (end-fn iter) [w_hidden_new w_out_new]
+      (let [activations
+            (mlp-forward inputs weights (:forward act-fns))
+            new-weights
+            (mlp-backward inputs targets activations
+                          weights (:backward act-fns))]
+        (if (end-fn iter) new-weights
             (let [new_order (shuffle (range (nrow inputs)))]
-              (recur w_hidden_new w_out_new
+              (recur new-weights
                      ($ new_order :all inputs)
                      ($ new_order :all targets)
                      (+ 1 iter))))))))
