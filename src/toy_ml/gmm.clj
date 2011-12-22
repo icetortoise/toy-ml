daac8fe8a1ddd73ed9ab40d987224c37a521cfb0
Andy Wu <icetortoise@gmail.com>
2011-12-22 17:10:31 +0800

    now gmm supports high dimention data

--- src/toy_ml/gmm.clj
+++ src/toy_ml/gmm.clj
@@ -1,32 +1,38 @@
 (ns toy-ml.gmm
-  (:use [incanter core distributions charts]))
+  (:use [incanter core distributions charts]
+        [toy-ml core])
+  (:require [incanter.stats :as stats]))
 
 (defn infinite-draw [d]
   (lazy-seq (cons (draw d)
                   (infinite-draw d))))
 
-(defn- gen-gaussians [m-s-list]
-  (vec (map (fn [[mean sigma]]
-              (normal-distribution mean sigma))
-            m-s-list)))
+(defn- mix [means sigmas n]
+  (->> (map #(stats/sample-mvn n :mean %1 :sigma %2)
+           means sigmas)
+       (apply conj-rows)
+       randomize))
 
-(defn generate-mixture [N mean-sigma-list]
-  (let [distributions (gen-gaussians mean-sigma-list)]
-        (for [n (range N)]
-          (draw (nth distributions
-                     (draw (integer-distribution
-                            (count distributions))))))))
+(defn generate-mixture [N & {:keys [means sigmas]}]
+  (let [ds (mix means sigmas (inc (/ N (count means))))]
+    (to-matrix ($ (range N) :all ds))))
+
+(defn- multivariant-normal-pdf [mean sigma x]
+  (let [diff ($= x - mean)]
+    ($= (exp ($= ($= (trans diff) <*> (solve sigma) <*> diff) / (minus 2)))
+        / (mult (pow ($= Math/PI * 2) ($= (count x) / 2))
+                (pow (det sigma) (/ 1 2))))))
 
 (defn GMM-EM [y n-mixture]
   (defn- params-init []
-    [(repeatedly n-mixture (fn [] (draw y)))
-     (repeat n-mixture (sum ($= ($= y - (mean y)) ** 2)))
+    [(repeatedly n-mixture (fn [] (trans (draw y))))
+     (repeat n-mixture (stats/covariance y))
      (repeat n-mixture (/ 1 n-mixture))])
   
   (defn- compute-gamma [params x]
     (let [[mu-coll sigma-coll pi-coll] params
           gamma-seq (map (fn [mu sigma pi]
-                           ($= pi * (exp ($= (minus ($= ($= x - mu) ** 2)) / sigma))))
+                           ($= pi * (multivariant-normal-pdf mu sigma (trans x))))
                          mu-coll sigma-coll pi-coll)
           gamma-sum (sum gamma-seq)]
       (map (fn [g] (/ g gamma-sum)) gamma-seq)))
@@ -37,11 +43,15 @@
     (let [mat-gammas ((comp trans matrix) gammas)
           y-trans (trans y)
           mu-coll (map (fn [gamma]
-                         ($= (sum ($= gamma * y-trans)) / (sum gamma)))
+                         (matrix ($= ($= gamma <*> y) / (sum gamma))))
                        mat-gammas)
           sigma-coll (map (fn [gamma mu]
-                            ($= (sum ($= gamma * ($= ($= y-trans - mu) ** 2)))
-                                / (sum gamma)))
+                            (let [delta (minus y (matrix (repeat (count gamma) mu)))]
+                              ($= (apply plus (map #($= %1 * %2)
+                                                   gamma
+                                                   (map (fn [x] ($= (trans x) <*> x))
+                                                        delta)))
+                                / (sum gamma))))
                           mat-gammas mu-coll)
           pi-coll (map (fn [gamma]
                          ($= (sum gamma) / (count gamma)))
@@ -49,18 +59,19 @@
       [mu-coll sigma-coll pi-coll]))
   
   (defn- log-likelihoods [params]
-    (defn- pdf-seq [d y]
-      (map (partial pdf d) y))
     (let [[mu-coll sigma-coll pi-coll] params]
-      (sum (vectorize (map (fn [mu sigma pi]
-                             ($= pi * (log (pdf-seq (normal-distribution mu sigma) y))))
-                           mu-coll sigma-coll pi-coll)))))
+      (defn- ll-one [x]
+        (log (sum (map (fn [mu sigma pi]
+                         (multivariant-normal-pdf
+                          mu sigma (trans x)))
+                       mu-coll sigma-coll pi-coll))))
+      (sum (map ll-one y))))
   
   (defn- run-em [params]
     (let [gammas (compute-gammas params)]
       (lazy-seq (cons {:params params
                        :gammas gammas
                        :log-likelihoods (log-likelihoods params)}
-                      (run-em (new-params gammas))))))
+                       (run-em (new-params gammas))))))
   {:E compute-gammas :M new-params
    :infinite-run (fn [] (run-em (params-init)))})
